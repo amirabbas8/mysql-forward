267437c26098989dffc2e548f1ca6d7407ed15e8
Amir Abbas <am_882011@live.com>
2020-10-29 18:32:55 +0330

    init

--- install.sh
+++ install.sh
@@ -9,11 +9,6 @@ apk update
 # install mysqldump
 apk add mysql-client
 
-# install s3 tools
-apk add python py-pip
-pip install awscli
-apk del py-pip
-
 # install go-cron
 apk add curl
 curl -L --insecure https://github.com/odise/go-cron/releases/download/v0.0.6/go-cron-linux.gz | zcat > /usr/local/bin/go-cron
